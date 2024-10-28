ea69fe5fffbacd41147e57383827193740e8ecc0
temawm <temafroloff@gmail.com>
2024-10-28 11:25:59 +0300

    Completed_TaskN8

--- InterpretedProgramming/seventhTaskPython.py
+++ InterpretedProgramming/seventhTaskPython.py
@@ -5,7 +5,7 @@ def decrypt_char(c):
         index = alphabet.index(c)
         if c == 'я':
             return 'а'
-        return alphabet[index - 1]
+        return alphabet[index + 1]
     else:
         return c
 
@@ -22,3 +22,35 @@ input_filename = 'encrypted.txt'
 output_filename = 'decrypted.txt'
 
 decrypt_file(input_filename, output_filename)
+
+
+def count_pairs(input_file):
+    with open(input_file, 'r', encoding='utf-8') as f:
+        first_line = f.readline().strip().split()
+        N = int(first_line[0])
+        K = int(first_line[1])
+        ratings = [int(f.readline().strip()) for _ in range(N)]
+    ratings.sort()
+
+    left = 0
+    right = N - 1
+    count = 0
+
+    while left < right:
+        if ratings[left] + ratings[right] >= K:
+            count += (right - left)
+            right -= 1
+        else:
+            left += 1
+
+    return count
+
+# Пример использования
+input_file = 'ratings.txt'
+result = count_pairs(input_file)
+print(result)
+
+
+
+
+
